db9c881b443ac9ef8712c949ebdb8f014d0d2a1d
Andrea <andreazago1997@gmail.com>
2021-01-20 17:56:59 +0100

    Error card

--- pages/NewsListPage.js
+++ pages/NewsListPage.js
@@ -1,8 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, RefreshControl, ToastAndroid} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  RefreshControl,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import axios from 'axios';
 import CardNews from '../components/CardNews';
 import API_KEY from '../API_KEY';
+import LottieView from 'lottie-react-native';
+import Colors from '../style/Colors';
 
 const NewsListPage = ({route}) => {
   const [newsList, setNewsList] = useState([]);
@@ -28,39 +37,73 @@ const NewsListPage = ({route}) => {
         }
       })
       .catch(function (error) {
-        if (category == 'general') {
-          ToastAndroid.show('Impossibile ottenere i dati', ToastAndroid.SHORT);
-        }
+        setNewsList([]);
         setRefreshing(false);
       });
   };
 
-  return (
-    <FlatList
-      data={newsList}
-      renderItem={({item}) => (
-        <CardNews
-          title={item.title}
-          description={item.description}
-          url={item.url}
-          urlToImage={item.urlToImage}
-          publishedAt={item.publishedAt}
-          content={item.content}
+  if (newsList.length == 0 && !refreshing) {
+    return (
+      <View
+        style={{
+          flexDirection: 'column',
+          backgroundColor: Colors.primary,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 20,
+          margin: 20,
+          borderRadius: 5,
+        }}>
+        <Text style={{color: 'white', paddingTop: 20, fontSize: 24}}>
+          Impossibile caricare i dati
+        </Text>
+        <LottieView
+          source={require('../assets/38463-error.json')}
+          autoPlay
+          loop
+          style={{height: 300, width: 300, marginTop: -10}}
         />
-      )}
-      keyExtractor={(item) => item.url}
-      contentContainerStyle={styles.container}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={() => {
-            setRefreshing(true);
-            getAPIData();
-          }}
-        />
-      }
-    />
-  );
+        <TouchableOpacity
+          disabled={refreshing}
+          onPress={() => getAPIData()}
+          style={{
+            backgroundColor: 'white',
+            paddingHorizontal: 40,
+            paddingVertical: 16,
+            borderRadius: 5,
+          }}>
+          <Text style={{fontWeight: 'bold', fontSize: 16}}>Ricarica</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <FlatList
+        data={newsList}
+        renderItem={({item}) => (
+          <CardNews
+            title={item.title}
+            description={item.description}
+            url={item.url}
+            urlToImage={item.urlToImage}
+            publishedAt={item.publishedAt}
+            content={item.content}
+          />
+        )}
+        keyExtractor={(item) => item.url}
+        contentContainerStyle={styles.container}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => {
+              setRefreshing(true);
+              getAPIData();
+            }}
+          />
+        }
+      />
+    );
+  }
 };
 
 export default NewsListPage;
